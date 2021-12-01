31af16a9ffc3eac54432a393c7f4ca9d745fe065
swetha-11-srisai <swethachowdary857@gmail.com>
2021-12-01 13:03:40 -0800

    [Swetha] add :user valid first name

---
+++ UserRegisteration/UserRegisteration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UserRegisteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registeration form");
+            UserRegisterationForm user = new UserRegisterationForm();
+            bool result = user.ValidateFirstName();
+            Console.WriteLine("Name:"+result);
+        }
+    }
+}
---
+++ UserRegisteration/UserRegisteration/UserRegisterationForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegisteration
+{
+    class UserRegisterationForm
+    {
+        public string FIRSTNAME_REGEX = "^[A-Z]{1}[a-zA-Z]{3,}$";
+        public bool ValidateFirstName()
+        {
+            return Regex.IsMatch("Swetha",FIRSTNAME_REGEX);
+        }
+    }
+}
